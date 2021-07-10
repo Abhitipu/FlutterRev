def4c20bf92e2f86fb111facc2b41d07be8885f0
Abhitipu <abhinandan0316@gmail.com>
2021-07-10 11:04:26 +0530

    Explored decoration Boxes and Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,74 @@
 import 'package:flutter/material.dart';
-// this is imported so that we maty use the material edsign toolkit
-// Widget : Element :: class : class_instance
 
-void main() => runApp(new MyNewApp());
+void main() => runApp(new MyNewAppv2());
 
-class MyApp extends StatelessWidget {
-  // the build method always has to be overriden
+class MyNewAppv2 extends StatelessWidget {
+  const MyNewAppv2({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "My new app",
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        // swatch cant just take a single color : it takes a range from 50 to 900
+        primarySwatch: Colors.indigo,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      // created a separate class for home to clean things up
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  // This holds the immutable objects and calls the createState method!
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-  // Key is for uniquely identifying the widget (useful for situations like reordering)
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // necessary to call setState to re run the build method to reflect changes
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.purple[50],
+        appBar: AppBar(
+          title: Text("My Awesome App"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter, // this is the event listener
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class MyNewApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My awesome flutter app",
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Hello world!"),
-          ),
-          body: Center(
-              child: Column(
+        body: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image(
-                image: AssetImage("images/abhitipu.jpg"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage("images/abhitipu.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 10,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(180),
+                  width: 400,
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text("Abhinandan"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text("Flutter Developer"),
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Text(
+                    "Abhinandan\nFlutter Developer",
+                    style: const TextStyle(
+                      fontFamily: 'Lobster',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 10,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(45.0))),
+                  padding: const EdgeInsets.all(20),
+                ),
               ),
             ],
-          )),
-          backgroundColor: Colors.pink[50],
-        ));
+          ),
+        ),
+      ),
+    );
   }
 }
-
-/*
-Scaffold: just a screen
-A widget has different properties that youcan specify
-eg : Scaffold has 
-      1. appbar
-      2. Body
-      3. Floating action button etc.
-*/
